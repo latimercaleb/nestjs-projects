24c189f0f6cfcdb61efd9712ccebbe4894851f5a
Exia <nohitokaze@gmail.com>
2026-04-02 19:08:00 -0400

    Adding param to get specific item from list

--- equipment-shop/src/products/products.controller.ts
+++ equipment-shop/src/products/products.controller.ts
@@ -1,20 +1,36 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get, Header, Body, Post } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Header,
+    Body,
+    Post,
+    Param
+} from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { Product } from './product.model';
 @Controller('products')
 export class ProductsController {
     constructor(private productService: ProductsService) {}
-    @Get()
-    getProducts(): Product[]{
-        return this.productService.getProducts()
-    }
 
     @Get('sampleTypes')
     @Header('Content-Type','text/html')
     test2(): any{
         return {message: 'New data format'} //  Headers practice
     }
+
+    @Get()
+    getProducts(): Product[]{
+        return this.productService.getProducts()
+    }
+
+    @Get(':id')
+    getProduct(@Param('id') id: string) {
+        console.log(id)
+        //return this.productService.getProducts()
+    }
+
+
 
     @Post()
     generateProducts(@Body('title') productTitle: string, @Body('description') desc: string, @Body('price') amt: number){// Using the Body param to fetch request params
