8675d85976030d5a84e69858b2b3ae2e690e1846
Exia <nohitokaze@gmail.com>
2026-04-03 02:05:52 -0400

    Adding update, needs to be tested before moving on

--- equipment-shop/src/products/products.controller.ts
+++ equipment-shop/src/products/products.controller.ts
@@ -1,39 +1,42 @@
 /* eslint-disable prettier/prettier */
-import {
-    Controller,
-    Get,
-    Header,
-    Body,
-    Post,
-    Param
-} from '@nestjs/common';
-import { ProductsService } from './products.service';
-import { Product } from './product.model';
+import {Controller, Get, Header, Body, Post, Param, Put, NotFoundException} from '@nestjs/common'
+import {ProductsService} from './products.service'
+import {Product} from './product.model'
 @Controller('products')
 export class ProductsController {
-    constructor(private productService: ProductsService) {}
+  constructor(private productService: ProductsService) {}
 
-    @Get('sampleTypes')
-    @Header('Content-Type','text/html')
-    test2(): any{
-        return {message: 'New data format'} //  Headers practice
-    }
+  @Get('sampleTypes')
+  @Header('Content-Type', 'text/html')
+  test2(): any {
+    return {message: 'New data format'} //  Headers practice
+  }
 
-    @Get()
-    getProducts(): Product[]{
-        return this.productService.getProducts()
-    }
+  @Get()
+  getProducts(): Product[] {
+    return this.productService.getProducts()
+  }
 
-    @Get(':id')
-    getProduct(@Param('id') id: string) {
-        console.log(id)
-        //return this.productService.getProducts()
-    }
+  @Get(':id')
+  getProduct(@Param('id') id: string): Product | NotFoundException {
+    // TODO: Test this
+    console.log(id)
+    return this.productService.getProduct(id)
+  }
 
+  @Put()
+  updateProduct(@Param('id') id: string, @Body() productData: Product) {
+        // TODO: Test this
+    return this.productService.updateProduct(id, productData)
+  }
 
-
-    @Post()
-    generateProducts(@Body('title') productTitle: string, @Body('description') desc: string, @Body('price') amt: number){// Using the Body param to fetch request params
-        return this.productService.addProduct(productTitle, desc, amt)
-    }
+  @Post()
+  generateProducts(
+    @Body('title') productTitle: string,
+    @Body('description') desc: string,
+    @Body('price') amt: number
+  ) {
+    // Using the Body param to fetch request params
+    return this.productService.addProduct(productTitle, desc, amt)
+  }
 }
--- equipment-shop/src/products/products.service.ts
+++ equipment-shop/src/products/products.service.ts
@@ -1,24 +1,45 @@
-/* eslint-disable prettier/prettier */
-import { Injectable } from '@nestjs/common';
-import { Product } from './product.model';
+import {Injectable, NotFoundException} from '@nestjs/common'
+import {Product} from './product.model'
 
 @Injectable()
 export class ProductsService {
-    productStore: Product[] = []
+  productStore: Product[] = []
 
-    generateID(): number {
-        return Math.floor(1000 + Math.random() * 9000);
-    }
+  generateID(): number {
+    return Math.floor(1000 + Math.random() * 9000)
+  }
 
-    addProduct(title: string, desc: string, price: number) {
-        console.log(`Params are: ${title} ${desc} ${price}`)
-        const id =  `${price + this.generateID()}__${title.slice(0,2)}`
-        const product = new Product(id, title,desc,price)
-        this.productStore.push(product)
-        return {new_product_id: product.id};
-    }
+  addProduct(title: string, desc: string, price: number) {
+    console.log(`Params are: ${title} ${desc} ${price}`)
+    const id = `${price + this.generateID()}__${title.slice(0, 2)}`
+    const product = new Product(id, title, desc, price)
+    this.productStore.push(product)
+    return {new_product_id: product.id}
+  }
 
-    getProducts(): Product[] {
-        return [...this.productStore]
+  getProducts(): Product[] {
+    return [...this.productStore]
+  }
+
+  getProduct(productId: string): Product | NotFoundException {
+    // TODO: Find product and return it, if no product return exception
+    const foundProduct = this.productStore.find((product) => product.id === productId)
+    return foundProduct ?? new NotFoundException('Product ID unidentified') // Trying this short-circuit flow
+  }
+
+  updateProduct(
+    id: string,
+    productToUpdate: {title?: string; description?: string; price?: number}
+  ) {
+    // Decide if ID is in list, if it is update the item and return a success message
+    const idxToUpdate = this.productStore.findIndex((product) => product.id === id)
+    if (idxToUpdate) {
+      this.productStore[idxToUpdate] = {
+        ...this.productStore[idxToUpdate],
+        ...productToUpdate
+      }
+    } else {
+      throw new NotFoundException('Invalid ID')
     }
+  }
 }
