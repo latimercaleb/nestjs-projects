d52a388c672cde37f9bbc0b14ca9ed84e590d4eb
Exia <nohitokaze@gmail.com>
2026-04-05 11:48:30 -0400

    Done with Crud for now, need to practice pipes

--- equipment-shop/src/app.controller.ts
+++ equipment-shop/src/app.controller.ts
@@ -1,17 +1,39 @@
-import {Controller, Get, Query, Param} from '@nestjs/common'
+import {Controller, Get, Query, Param, HttpCode, ParseIntPipe, ParseFloatPipe, ParseBoolPipe} from '@nestjs/common'
 import {AppService} from './app.service'
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    console.log('Sweet')
-    return this.appService.getHello()
+  // TODO: Pull pipes into its own controller, write your own service layer for it
+  @Get('/pipes/:id')
+  getId(@Param('id', ParseIntPipe) id): any {
+    return {
+      message: id
+    }
   }
 
+  @Get('/decimalPipe/:id')
+  getDecimalId(@Param('id', ParseFloatPipe) id): any {
+    return {
+      message: id
+    }
+  }
+
+  @Get('/booleanPipe/:id')
+  geBooleanId(@Param('id', ParseBoolPipe) id): any {
+    return {
+      message: id
+    }
+  }
+
+  // @Get('/arrayPipe') // TODO This throws error, look into later
+  // getArrayId(@Query('num', ParseArrayPipe) nums) {
+  //   return nums
+  // }
+
   @Get(':id')
+  @HttpCode(202)
   simpleQueryParam(
     @Param('id') id: number,
     @Query('name') name: string,
