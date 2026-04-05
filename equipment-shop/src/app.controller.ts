509c4ff8b57b32e858c1414b19c509c7a757268b
Exia <nohitokaze@gmail.com>
2026-04-05 10:27:20 -0400

    Working with query params

--- equipment-shop/src/app.controller.ts
+++ equipment-shop/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -9,5 +9,10 @@ export class AppController {
   getHello(): string {
     console.log('Sweet')
     return this.appService.getHello();
+  }
+
+  @Get()
+  simpleQueryParam(@Query('name') name: string) {
+    return `The name sent is ${name}`;
   }
 }
