809c0bdb4fa809d6055e0f24a1512e8c7a104964
Exia <nohitokaze@gmail.com>
2026-04-09 17:33:00 -0400

    Adding validator logic

--- equipment-shop/src/app.controller.ts
+++ equipment-shop/src/app.controller.ts
@@ -1,9 +1,38 @@
-import {Controller, Get, Query, Param, HttpCode, ParseIntPipe, ParseFloatPipe, ParseBoolPipe} from '@nestjs/common'
+import {
+  Controller,
+  Get,
+  Query,
+  Param,
+  HttpCode,
+  ParseIntPipe,
+  ParseFloatPipe,
+  ParseBoolPipe,
+  ParseArrayPipe,
+  ParseUUIDPipe,
+  Body,
+  Post,
+  UsePipes,
+  ValidationPipe
+} from '@nestjs/common'
+import { IsEmail,IsAlphanumeric } from 'class-validator'
 import {AppService} from './app.service'
-
+class AuthDTO {
+  @IsEmail()
+  email: string;
+  @IsAlphanumeric()
+  password: string;
+}
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Post('/auth/register')
+  @UsePipes(ValidationPipe)
+  registerUser(@Body() userData: AuthDTO) {
+    return {
+      Email: `Registered Email is: ${userData.email}`
+    }
+  }
 
   // TODO: Pull pipes into its own controller, write your own service layer for it
   @Get('/pipes/:id')
@@ -27,10 +56,16 @@ export class AppController {
     }
   }
 
-  // @Get('/arrayPipe') // TODO This throws error, look into later
-  // getArrayId(@Query('num', ParseArrayPipe) nums) {
-  //   return nums
-  // }
+  @Get('/arrayPipe') // Notes this requires validation pipe to be imported via pckg json and added to main.ts
+  getArrayId(@Query('num', ParseArrayPipe) nums) {
+    return nums
+  }
+
+  @Get('/uuid/:id')
+  uuID(@Param('id', new ParseUUIDPipe({version: '4'})) id) {
+    // 92fabb7e-7314-4938-beeb-1ab855c98767 to test uuid, any other id will be invalid, can configure to test specific uuid in constructor
+    return {uuid: id, status: 200}
+  }
 
   @Get(':id')
   @HttpCode(202)
--- equipment-shop/src/app.module.ts
+++ equipment-shop/src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsController } from './products/products.controller';
 import { ProductsService } from './products/products.service';
+import { PipesController } from './pipes/pipes.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, ProductsController],
+  controllers: [AppController, ProductsController, PipesController],
   providers: [AppService, ProductsService],
 })
 export class AppModule {}
