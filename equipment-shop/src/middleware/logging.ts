19d38f2efc235ec98af8cd0d91d3e4670c79081c
Exia <nohitokaze@gmail.com>
2026-04-14 01:37:52 -0400

    Adding middleware, and  custompipe

---
+++ equipment-shop/src/middleware/logging.ts
@@ -0,0 +1,9 @@
+import {ArgumentMetadata, BadRequestException, Injectable, NestMiddleware, } from '@nestjs/common'
+import { NextFunction, Request, Response } from 'express';
+@Injectable()
+export class LoggingMiddleware implements NestMiddleware {
+    use(req: Request, res: Response, next: NextFunction) {
+       console.log(`Date of middleware log: ${new Date().toISOString}`)
+    }
+   
+}
